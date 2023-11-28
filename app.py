8d6f0be5533dd68a0c8ce00de35694c6eadd762b
M-m-mBootz <55406524+M-m-mBootz@users.noreply.github.com>
2023-11-28 10:25:07 +0100

    Update app.py
    centering on page

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
 import streamlit as st
 
 # Set the page config to widen the app, and set a title and icon
-st.set_page_config(page_title='GenAI Tool', layout='wide')
+st.set_page_config(page_title='GenAI Tool', layout='centered')
 
-# Title of your app
-st.title("GenAI Tool")
+# Use columns to center the content
+# The first and last columns act as margins
+left_column, center_column, right_column = st.columns([1, 2, 1])
 
-# Instructions
-st.write("Enter your prompt in the text box below and hit enter to see the magic.")
+# Use the center column for your content
+with center_column:
+    # Title of your app
+    st.title("GenAI Tool")
 
-# Text input box at the bottom
-user_input = st.text_input("Prompt", key="prompt_input")
+    # Instructions
+    st.write("Enter your prompt in the text box below and hit enter to see the magic.")
 
-# You can now use the user_input variable to pass to your genAI tool
-# For example:
-# if user_input:
-#    response = your_genai_function(user_input)
-#    st.write(response)
+    # Text input box at the bottom
+    user_input = st.text_input("Prompt", key="prompt_input")
 
-# You can put this at the end if you want to display something once the user enters a prompt
-if user_input:
-    st.write("You entered: ", user_input)
+    # You can now use the user_input variable to pass to your genAI tool
+    # For example:
+    # if user_input:
+    #    response = your_genai_function(user_input)
+    #    st.write(response)
 
-# The rest of your genAI tool logic will go here
+    # Display something once the user enters a prompt
+    if user_input:
+        st.write("You entered: ", user_input)
+
+    # The rest of your genAI tool logic will go here
+
