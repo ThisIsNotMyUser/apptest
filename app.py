5ef1acb5f268eb68493bda0446a35be29b42aa06
M-m-mBootz <55406524+M-m-mBootz@users.noreply.github.com>
2023-11-28 11:50:33 +0100

    Update app.py
    style changes and background

--- app.py
+++ app.py
@@ -1,71 +1,36 @@
 import streamlit as st
 
 # Set the page config to widen the app, and set a title and icon
-st.set_page_config(page_title='SCoP', layout='centered')
+st.set_page_config(page_title='GenAI Tool', layout='centered')
 
 # URL of the background image (replace with the actual URL of your image in GitHub)
 bg_image_url = "https://github.com/M-m-mBootz/apptest/blob/main/bg.png"
 
 # Define the custom HTML and CSS to use
-html_string = f"""
+html_string = """
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
 
         body {
             font-family: 'Montserrat', sans-serif;
-            background-image: url({bg_image_url});
+            background-image: url('""" + bg_image_url + """'); /* URL is added here */
             background-size: cover;
             color: white; /* White text color */
         }
 
         /* ... rest of your CSS ... */
 
-        /* Custom CSS to change the placeholder color */
-        ::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
-            color: white;
-            opacity: 1; /* Firefox */
-        }
-
-        :-ms-input-placeholder { /* Internet Explorer 10-11 */
-            color: white;
-        }
-
-        ::-ms-input-placeholder { /* Microsoft Edge */
-            color: white;
-        }
+        /* Unfortunately, Streamlit does not support changing the placeholder color via CSS */
     </style>
 
     <div class="logo">
-        <span>S</span><span>C</span><span>o</span><span>P</span><span>I</span>
-        <span>L</span><span>O</span><span>T</span>
+        <span>G</span><span>e</span><span>n</span><span>A</span><span>I</span>
+        <span>T</span><span>o</span><span>o</span><span>l</span>
     </div>
 """
 
 # Use the HTML string with markdown to create the logo with the hover effect
 st.markdown(html_string, unsafe_allow_html=True)
-
-# Inject the Montserrat font into the Streamlit app
-st.markdown(
-    """
-    <style>
-        html, body, [class*="st-"], .stTextInput, .st-bb, .st-da, .st-ea, .css-10trblm {
-            font-family: 'Montserrat', sans-serif;
-            color: white;
-        }
-        .stTextInput input, .stTextInput input::placeholder {
-            color: white;
-        }
-        .stTextInput>div>div>input {
-            border-color: white;
-        }
-        .css-1cpxqw2 {
-            border-color: white;
-        }
-        /* Add other Streamlit components you wish to style */
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 # Text input box at the bottom with placeholder
 user_input = st.text_input("", placeholder="Ask SCoP", key="prompt_input")
@@ -75,6 +40,7 @@ if user_input:
     st.write("You entered: ", user_input)
 
 # The rest of your genAI tool logic will go here
+
 
 
 
