3709c22c99ec5681b91e9418f5671f2b8b2296c6
M-m-mBootz <55406524+M-m-mBootz@users.noreply.github.com>
2023-11-28 12:17:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
 # app.py
 import streamlit as st
 
-# Use columns to center the content
-# Create three columns with the middle one being the main one
-col1, col2, col3 = st.columns([1, 2, 1])
+# Center the title, instructions, and input box using columns
+# Adjust the ratio to make the center column wider
+col1, col2, col3 = st.columns([1, 6, 1])
 
-# Use the middle column to display the title, instruction, and input box
+# Use the middle column to place the title, instructions, and text input
 with col2:
     st.title('SCoP')
-
-    # Instruction for the user on how to use the tool
     st.write('Please enter your prompt in the text box below and press Enter.')
-
-    # Text box for user input
     user_input = st.text_input("Enter your prompt here...", key="input")
 
-# Check if there's input and display the output above everything else
+# Check if there's input and display the output at the top of the page
 if user_input:
     # Display the output in a container at the top of the page
     with st.container():
@@ -23,6 +19,3 @@ if user_input:
 
     # Clear the input box after displaying the output
     st.session_state.input = ""
-
-
-
