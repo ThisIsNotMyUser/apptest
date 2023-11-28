3dd0a5a7f61ce21fb82d0eff8a567f9a008eb562
M-m-mBootz <55406524+M-m-mBootz@users.noreply.github.com>
2023-11-28 12:07:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,27 +7,29 @@ st.set_page_config(page_title='SCoP', layout='centered')
 html_string = """
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
+        
+        /* Add padding to the bottom of the page to lift the content up */
+        .reportview-container .main .block-container {
+            padding-bottom: 5rem;
+        }
 
-        body {
-            font-family: 'Montserrat', sans-serif;
-            background-color: #414141ff; /* Dark background */
-            color: #ffffff; /* White text color */
+        /* Style the logo and input box container */
+        .bottom-container {
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            padding: 1rem;
+            background-color: #414141ff;
         }
 
         .logo {
             font-family: 'Montserrat', sans-serif;
-            font-size: 2em;
+            font-size: 4em; /* 2 times larger */
             text-transform: uppercase;
-            color: #ffffff;
-        }
-
-        .logo span.large {
-            font-size: 1.5em; /* Larger size */
-            font-weight: 700;
-        }
-
-        .logo span.small {
-            font-size: 1em; /* Smaller size */
+            color: white;
+            display: flex;
+            justify-content: center;
         }
 
         .stTextInput input {
@@ -37,58 +39,50 @@ html_string = """
             border-color: white;
         }
 
-        .stTextInput>div>div>input {
+        /* Style focused input box */
+        .stTextInput input:focus {
             border-color: white;
+            box-shadow: none;
         }
+
+        /* Style the placeholder */
+        .stTextInput input::placeholder {
+            color: white;
+            opacity: 0.5;
+        }
+        
+        /* Adjust the style of the input container */
+        .stTextInput {
+            width: 100%;
+            padding: 0 1rem;
+        }
+        
+        /* Add custom styles for Streamlit components */
         .css-1cpxqw2 {
             border-color: white;
         }
-        /* Add other Streamlit components you wish to style */
     </style>
 
-    <div class="logo">
-        <span class="large">S</span>
-        <span class="large">C</span>
-        <span class="small">O</span>
-        <span class="large">P</span>
+    <div class="bottom-container">
+        <div class="logo">SCoP</div>
+        <!-- This input box will be moved to Streamlit component area -->
     </div>
 """
 
-# Use the HTML string with markdown to create the logo with the hover effect
+# Inject custom styles and logo into the Streamlit app
 st.markdown(html_string, unsafe_allow_html=True)
 
-# Inject the Montserrat font into the Streamlit app
-st.markdown(
-    """
-    <style>
-        html, body, [class*="st-"], .stTextInput, .st-bb, .st-da, .st-ea, .css-10trblm {
-            font-family: 'Montserrat', sans-serif;
-            background-color: #414141ff;
-            color: white;
-        }
-        .stTextInput input {
-            color: white;
-        }
-        .stTextInput>div>div>input {
-            border-color: white;
-        }
-        .css-1cpxqw2 {
-            border-color: white;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+# Create a placeholder to display the result above the input box
+result_placeholder = st.empty()
 
 # Text input box with placeholder
 user_input = st.text_input("", placeholder="Ask SCoP", key="prompt_input")
 
-# Display something once the user enters a prompt
+# Display the result above the input box
 if user_input:
-    st.write("You entered: ", user_input)
+    result_placeholder.markdown(f"You entered: {user_input}", unsafe_allow_html=True)
 
 # The rest of your genAI tool logic will go here
-
 
 
 
