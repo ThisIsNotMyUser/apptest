d26005921b82818d58f26bb231cf74efcd39144c
M-m-mBootz <55406524+M-m-mBootz@users.noreply.github.com>
2023-11-28 12:15:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,86 +1,28 @@
+# app.py
 import streamlit as st
 
-# Set the page config to widen the app, and set a title and icon
-st.set_page_config(page_title='SCoP', layout='centered')
+# Use columns to center the content
+# Create three columns with the middle one being the main one
+col1, col2, col3 = st.columns([1, 2, 1])
 
-# Define the custom HTML and CSS to use
-html_string = """
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
+# Use the middle column to display the title, instruction, and input box
+with col2:
+    st.title('SCoP')
 
-        body {
-            font-family: 'Montserrat', sans-serif;
-            background-color: #414141ff; /* Dark background */
-            color: #ffffff; /* White text color */
-            margin: 0;
-            height: 100vh;
-            display: flex;
-            flex-direction: column;
-            justify-content: flex-end;
-            align-items: center;
-        }
+    # Instruction for the user on how to use the tool
+    st.write('Please enter your prompt in the text box below and press Enter.')
 
-        .logo {
-            font-family: 'Montserrat', sans-serif;
-            font-size: 2em;
-            text-transform: uppercase;
-            color: #ffffff;
-        }
+    # Text box for user input
+    user_input = st.text_input("Enter your prompt here...", key="input")
 
-        .logo span.large {
-            font-size: 1.5em; /* Larger size */
-            font-weight: 700;
-        }
-
-        .logo span.small {
-            font-size: 1em; /* Smaller size */
-        }
-
-        /* Streamlit's specific text input styling */
-        .stTextInput input {
-            font-family: 'Montserrat', sans-serif;
-            background-color: #414141ff;
-            color: white;
-            border: 1px solid white;
-        }
-
-        /* Override default Streamlit styling for focused input */
-        .stTextInput input:focus {
-            border: 1px solid white;
-            outline: none;
-        }
-        
-        .stButton>button {
-            font-family: 'Montserrat', sans-serif;
-            color: white;
-            background-color: #414141ff;
-            border-color: white;
-        }
-    </style>
-
-    <div class="logo">
-        <span class="large">S</span>
-        <span class="large">C</span>
-        <span class="small">O</span>
-        <span class="large">P</span>
-    </div>
-"""
-
-# Use the HTML string with markdown to create the logo with the hover effect
-st.markdown(html_string, unsafe_allow_html=True)
-
-# Text input box with placeholder
-user_input = st.text_input("", placeholder="Ask SCoP", key="prompt_input")
-
-# Display the result above the text box
+# Check if there's input and display the output above everything else
 if user_input:
-    st.empty()  # This is where you would display the result
-    st.write("You entered: ", user_input)
+    # Display the output in a container at the top of the page
+    with st.container():
+        st.write(f'You entered: {user_input}')
 
-# The rest of your genAI tool logic will go here
-
-
-
+    # Clear the input box after displaying the output
+    st.session_state.input = ""
 
 
 
