b29ab9ab3b106d23ce68d464da18674b1e8188a9
M-m-mBootz <55406524+M-m-mBootz@users.noreply.github.com>
2023-11-28 10:29:13 +0100

    Update app.py
    adding css

--- app.py
+++ app.py
@@ -3,30 +3,68 @@ import streamlit as st
 # Set the page config to widen the app, and set a title and icon
 st.set_page_config(page_title='GenAI Tool', layout='centered')
 
-# Use columns to center the content
-# The first and last columns act as margins
-left_column, center_column, right_column = st.columns([1, 2, 1])
+# Define the custom HTML and CSS to use
+html_string = """
+    <style>
+        .logo {
+            font-family: 'Arial', sans-serif;
+            font-size: 2em;
+            text-transform: uppercase;
+            color: #3498db;
+            cursor: pointer;
+        }
 
-# Use the center column for your content
-with center_column:
-    # Title of your app
-    st.title("GenAI Tool")
+        .logo span {
+            display: inline-block;
+        }
 
-    # Instructions
-    st.write("Enter your prompt in the text box below and hit enter to see the magic.")
+        .logo span:hover {
+            animation: distort 0.5s ease-in-out infinite;
+        }
 
-    # Text input box at the bottom
-    user_input = st.text_input("Prompt", key="prompt_input")
+        @keyframes distort {
+            0% {
+                transform: skewX(0deg) skewY(0deg);
+            }
+            25% {
+                transform: skewX(10deg) skewY(10deg);
+            }
+            50% {
+                transform: skewX(-10deg) skewY(-10deg);
+            }
+            75% {
+                transform: skewX(5deg) skewY(5deg);
+            }
+            100% {
+                transform: skewX(0deg) skewY(0deg);
+            }
+        }
+    </style>
 
-    # You can now use the user_input variable to pass to your genAI tool
-    # For example:
-    # if user_input:
-    #    response = your_genai_function(user_input)
-    #    st.write(response)
+    <div class="logo">
+        <span>S</span><span>C</span><span>O</span><span>P</span><span>I</span><span>L</span><span>O</span><span>T</span>
+    </div>
+"""
 
-    # Display something once the user enters a prompt
-    if user_input:
-        st.write("You entered: ", user_input)
+# Use the HTML string with markdown to create the logo with the hover effect
+st.markdown(html_string, unsafe_allow_html=True)
 
-    # The rest of your genAI tool logic will go here
+# Instructions
+st.write("Enter your prompt in the text box below and hit enter to see the magic.")
+
+# Text input box at the bottom
+user_input = st.text_input("Prompt", key="prompt_input")
+
+# You can now use the user_input variable to pass to your genAI tool
+# For example:
+# if user_input:
+#    response = your_genai_function(user_input)
+#    st.write(response)
+
+# Display something once the user enters a prompt
+if user_input:
+    st.write("You entered: ", user_input)
+
+# The rest of your genAI tool logic will go here
+
 
