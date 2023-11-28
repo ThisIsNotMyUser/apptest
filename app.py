b6573eaf40c9fef2c75a58b786bab4255d9e0767
M-m-mBootz <55406524+M-m-mBootz@users.noreply.github.com>
2023-11-28 11:55:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ import streamlit as st
 # Set the page config to widen the app, and set a title and icon
 st.set_page_config(page_title='GenAI Tool', layout='centered')
 
-# URL of the background image (replace with the actual URL of your image in GitHub)
-bg_image_url = "https://github.com/M-m-mBootz/apptest/blob/main/bg.png"
-
 # Define the custom HTML and CSS to use
 html_string = """
     <style>
@@ -13,14 +10,78 @@ html_string = """
 
         body {
             font-family: 'Montserrat', sans-serif;
-            background-image: url('""" + bg_image_url + """'); /* URL is added here */
-            background-size: cover;
+            background-color: #414141ff; /* Dark background */
             color: white; /* White text color */
         }
 
-        /* ... rest of your CSS ... */
+        .logo {
+            font-family: 'Montserrat', sans-serif;
+            font-size: 4em; /* 2 times larger */
+            text-transform: uppercase;
+            color: white; /* White text color for the logo */
+            cursor: pointer;
+        }
 
-        /* Unfortunately, Streamlit does not support changing the placeholder color via CSS */
+        .logo span {
+            display: inline-block;
+        }
+
+        .logo span:hover {
+            animation: distort 0.5s ease-in-out infinite;
+        }
+
+        @keyframes distort {
+            0% {
+                transform: skewX(0deg) skewY(0deg);
+            }
+            25% {
+                transform: skewX(10deg) skewY(10deg);
+            }
+            50% {
+                transform: skewX(-10deg) skewY(-10deg);
+            }
+            75% {
+                transform: skewX(5deg) skewY(5deg);
+            }
+            100% {
+                transform: skewX(0deg) skewY(0deg);
+            }
+        }
+
+        /* Change Streamlit components to the dark theme */
+        .stTextInput .st-bb, .stTextInput .st-eb, .stTextInput .css-1cpxqw2 {
+            background-color: #414141ff;
+            color: white;
+            border-color: white;
+        }
+
+        /* This targets all Streamlit text */
+        .stTextInput, .st-bb, .st-da, .st-ea, .css-10trblm {
+            font-family: 'Montserrat', sans-serif;
+            color: white;
+        }
+
+        /* Override default Streamlit styling for other elements */
+        .st-bv, .st-bw, .st-bx, .stButton>button {
+            background-color: #414141ff;
+            color: white;
+            border-color: white;
+        }
+
+        /* Placeholder text color */
+        .stTextInput input::placeholder {
+            color: white;
+            opacity: 1; /* Make it fully opaque */
+        }
+
+        .stTextInput input::-webkit-input-placeholder {
+            color: white;
+            opacity: 1;
+        }
+
+        .stTextInput input {
+            color: white;
+        }
     </style>
 
     <div class="logo">
@@ -40,6 +101,7 @@ if user_input:
     st.write("You entered: ", user_input)
 
 # The rest of your genAI tool logic will go here
+
 
 
 
