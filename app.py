885449d110ec6139f93ae8e3fe46badf6a4e56e1
M-m-mBootz <55406524+M-m-mBootz@users.noreply.github.com>
2023-11-28 12:21:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,90 @@
 # app.py
 import streamlit as st
 
-# Center the title, instructions, and input box using columns
-# Adjust the ratio to make the center column wider
-col1, col2, col3 = st.columns([1, 6, 1])
+# Define the HTML and CSS
+html_content = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    body {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+      background-color: #414141ff;
+    }
+    .logo {
+      font-family: 'Montserrat', sans-serif;
+      font-size: 2em;
+      text-transform: uppercase;
+      position: relative;
+      color: #ffffff;
+    }
+    .logo span {
+      display: inline-block;
+      transition: transform 0.3s ease-in-out;
+    }
+    .logo span.large {
+      font-size: 1.5em;
+      font-weight: 700;
+    }
+    .logo span.small {
+      font-size: 1em;
+    }
+    .logo span.hidden {
+      opacity: 0;
+      transition: opacity 0.5s ease-in-out;
+    }
+    .logo:hover .small {
+      opacity: 1;
+      transition: opacity 1s ease-in-out;
+    }
+    .magnifyspan {
+      animation: magnify 0.5s ease-in-out infinite;
+    }
+    .logo span.animated {
+      animation: moveMagnify 5s linear alternate;
+    }
+    .logo span:hover {
+      transform: scale(1.2);
+    }    
+    @keyframes magnify {
+      0% {
+        transform: scale(1);
+      }
+      50% {
+        transform: scale(1.2);
+      }
+      100% {
+        transform: scale(1);
+      }
+    }
+  </style>
+</head>
+<body>
 
-# Use the middle column to place the title, instructions, and text input
-with col2:
-    st.title('SCoP')
-    st.write('Please enter your prompt in the text box below and press Enter.')
-    user_input = st.text_input("Enter your prompt here...", key="input")
+<div class="logo">
+  <span class="large">S</span>
+  <span class="large">C</span>
+  <span class="small">O</span>
+  <span class="large">P</span>
+  <span class="small hidden">i</span>
+  <span class="small hidden">l</span>
+  <span class="small hidden">o</span>
+  <span class="small hidden">t</span>
+</div>
 
-# Check if there's input and display the output at the top of the page
-if user_input:
-    # Display the output in a container at the top of the page
-    with st.container():
-        st.write(f'You entered: {user_input}')
+</body>
+</html>
+'''
+
+# Using st.markdown to render HTML content
+st.markdown(html_content, unsafe_allow_html=True)
+
+# Continue with the rest of your Streamlit content
+# ...
+
